a9b2b63f89c036be4d895347a9573cd47ea71a58
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2020-09-16 12:51:17 +1200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-//And This Is Java/Python!
-//All The Code Is There!
 //If You Want To Edit Anything Goto "custom.ts"
+//All The Code Is There!
+//And This Is Java/Python!
 "The Code Is Writen In Java Script So There Are No Blocks"
 "BitSci Was Writen In Java Script"
 "Made By DrMeepso"
